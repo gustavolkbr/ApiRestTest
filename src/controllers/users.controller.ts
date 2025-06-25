5e8e4dff479abdf8789226ceedf01d81cf508a7a
gustavolkbr <gustavo.lnkbr@gmail.com>
2025-06-25 16:24:41 -0300

    validation with celebrate

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
-import { ValidationError } from "../errors/validation.error";
 import { NotFoundError } from "../errors/not-found.error";
 
 
@@ -38,12 +37,8 @@ export class UsersController {
 
     static async save(req: Request, res: Response, next: NextFunction) {
         let user = req.body;
-
-            if(!user.email || user.email.length === 0){
-                throw new ValidationError ("E-mail obrigatório");
-            }
-            
-            const userSalvo = await getFirestore().collection("users").add(user);
+       
+        const userSalvo = await getFirestore().collection("users").add(user);
         res.status(201).send({
             message: `usuário ${userSalvo.id} criado com sucesso!!!`
         })
