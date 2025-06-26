0b39f7cb5b1664229010c5f7a543ca0fd2666c65
gustavolkbr <gustavo.lnkbr@gmail.com>
2025-06-25 22:45:54 -0300

    refactor: move db access to repositories layer

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,50 @@
+import { CollectionReference, getFirestore } from "firebase-admin/firestore";
+import { User } from "../models/user.model";
+import { userInfo } from "os";
+
+export class UserRepository {
+
+    private collection: CollectionReference;
+
+    constructor() {
+        this.collection = getFirestore().collection("users");
+    }
+
+    async getAll(): Promise <User[]> {
+        const snapShot = await this.collection.get();
+            return snapShot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data() 
+                };
+            }) as User[];
+    }
+
+    async getById(id: string): Promise <User | null> {
+        const doc = await this.collection.doc().get();
+        if (doc.exists) {
+            return {
+                id: doc.id,
+                ...doc.data()
+            } as User; 
+        } else {
+            return null;
+        }
+    }
+
+    async save(user: User): Promise <void> {
+        await this.collection.add(user);
+    }
+
+    async update (user:User) {
+        let docRef = this.collection.doc(user.id);
+        await docRef.set({
+                nome: user.nome,
+                email: user.email
+        });
+    }
+
+    async delete(id:string): Promise<void> {
+        await this.collection.doc(id).delete();
+    }
+}
